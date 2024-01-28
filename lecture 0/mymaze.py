eccb6b67174b2e50a449872e0843517ffd89f602
Sama mohamed mahmoud abd ellatif <sama.mohamed@std.mans.edu.eg>
2024-01-28 12:01:12 +0200

    second commit

---
+++ lecture 0/mymaze.py
@@ -0,0 +1,46 @@
+import sys
+
+class node:
+    def __init__(self,state,parent,action ) -> None:
+        self.parent = parent
+        self.state = state
+        self.action = action
+
+class StackFrontier:
+    #4 dfs 
+    def __init__(self) -> None:
+        frontier = []
+
+    def add(self, node):
+        self.frontier.append(node)
+
+    def contain_state(self, state):
+        return any(node.state == state for node in self.frontier)
+    
+    def empty(self):
+        return len(self.frontier)==0
+    
+    def remove(self):
+        if len(self.frontier)==0:
+            raise Exception("empty frontier")
+        else :
+            node = self.frontier[-1]
+            self.frontier = self.frontier[:-1] 
+            return node
+        
+class queueFrontier(StackFrontier):
+
+    def remove(self):
+        if len(self.frontier)==0:
+            raise Exception("empty frontier")
+        else :
+            node = self.frontier[0]
+            self.frontier = self.frontier[1:]
+            return node
+        
+class maze:
+
+    def __init__(self) -> None:
+        with open(filename) as f:
+            pass
+        
